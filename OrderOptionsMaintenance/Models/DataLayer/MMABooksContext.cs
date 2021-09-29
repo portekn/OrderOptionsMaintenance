1bd552c59f903af106d716c846de1fc30c20aa11
Kelli Porter <Portekn@mail.uc.edu>
2021-09-29 15:54:02 -0400

    Moved Connection String

--- OrderOptionsMaintenance/Models/DataLayer/MMABooksContext.cs
+++ OrderOptionsMaintenance/Models/DataLayer/MMABooksContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using System.Configuration;
 
 #nullable disable
 
@@ -28,8 +29,7 @@ namespace OrderOptionsMaintenance.Models.DataLayer
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB; AttachDBFilename=C:\\Users\\Vampi\\Utilities\\Desktop\\School\\Fall 2021\\5. Contemporary Programming\\4.Entity Framework Core Database First\\OrderOptionsMaintenance\\OrderOptionsMaintenance\\MMABooks.mdf; Integrated Security=True");
+            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["MMABooks"].ConnectionString);
             }
         }
 
